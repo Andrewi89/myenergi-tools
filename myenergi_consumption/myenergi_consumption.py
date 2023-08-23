592a190fbda527b535ea4765d66ab2fe0b89beeb
Andrewi89 <Andrew.ireland1@live.co.uk>
2023-08-23 21:58:11 +0100

    refactored code, added graphs, added start and end date, added CSV export

--- myenergi_consumption/myenergi_consumption.py
+++ myenergi_consumption/myenergi_consumption.py
@@ -1,53 +1,41 @@
+# Importing necessary libraries
 import streamlit as st
 import pandas as pd
 import requests
 from requests.auth import HTTPDigestAuth
-from datetime import datetime, timedelta
+from datetime import datetime
 
+# Constants
+BASE_URL = "https://s18.myenergi.net"
+DEVICES = ['zappi', 'eddi', 'harvi', 'libbi']
 
+# Function to get device data
 def get_device_data(device_serial, api_key):
-    url = f"https://s18.myenergi.net/cgi-jstatus-*"
+    url = f"{BASE_URL}/cgi-jstatus-*"
     response = requests.get(url, auth=HTTPDigestAuth(device_serial, api_key))
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    return response.json() if response.status_code == 200 else None
 
+# Function to get consumption data
+def get_consumption_data(device, device_serial, gateway_serial, api_key, start_date, end_date):
+    data = []
+    date_range = pd.date_range(start_date, end_date)
+    for date in date_range:
+        formatted_date = date.strftime("%Y-%m-%d")
+        url = f"{BASE_URL}/cgi-jday-{device}{device_serial}-{formatted_date}"
+        response = requests.get(url, auth=HTTPDigestAuth(gateway_serial, api_key))
+        if response.status_code == 200:
+            data.extend(response.json().get(f"U{device_serial}", []))
+    return data
 
-def get_consumption_data(device, device_serial, gateway_serial, api_key, selected_date):
-    formatted_date = selected_date.strftime("%Y-%m-%d")
-    url = f"https://s18.myenergi.net/cgi-jday-{device}{device_serial}-{formatted_date}"
-    print(url)
-    response = requests.get(url, auth=HTTPDigestAuth(gateway_serial, api_key))
-    if response.status_code == 200:
-        print(response.json())
-        return response.json().get(f"U{device_serial}", [])
-    else:
-        print("API Failed")
-        return None
-
-
+# Function to find device
 def find_device(device_data, user_serial):
     user_serial = int(user_serial)
-    all_devices = {}
-    matched_device = None
-
-    for data_dict in device_data:
-        if isinstance(data_dict, dict):
-            for device_name in ['zappi', 'eddi', 'harvi', 'libbi']:
-                if data_dict.get(device_name):
-                    all_devices[device_name] = data_dict[device_name][0].get(
-                        'sno')
-                    if all_devices[device_name] == user_serial:
-                        matched_device = device_name
-
+    all_devices = {device_name: data_dict[device_name][0].get('sno') for data_dict in device_data if isinstance(
+        data_dict, dict) for device_name in DEVICES if data_dict.get(device_name)}
+    matched_device = next((device for device, serial in all_devices.items() if serial == user_serial), None)
     return matched_device, all_devices
 
-
-def joules_to_wh(joules):
-    return joules / 3600
-
-
+# Main function
 def main():
     st.title("Myenergi Data")
     st.info('In order to get your consumption you will need to enter the Gateway Device Serial & API Key to find devices.', icon="ℹ️")
@@ -62,140 +50,128 @@ def main():
     device_serial = st.text_input("Gateway Device Serial:")
     api_key = st.text_input("Gateway API Key:")
 
-    if 'devices' not in st.session_state:
-        st.session_state.devices = []
+    st.session_state.setdefault('devices', [])
+    st.session_state.setdefault('device_to_serial_map', {})
+    st.session_state.setdefault('device_messages', [])
 
-    if 'device_to_serial_map' not in st.session_state:
-        st.session_state.device_to_serial_map = {}
-
-    if 'device_messages' not in st.session_state:
-        st.session_state.device_messages = []
-
-    if st.button("Find Devices"):
-        if device_serial and api_key:
+    if st.button("Find Devices") and device_serial and api_key:
+        with st.spinner('Fetching device data...'):
             device_data = get_device_data(device_serial, api_key)
-            matched_device, all_device_serials = find_device(
-                device_data, device_serial)
+        matched_device, all_device_serials = find_device(device_data, device_serial)
 
-            for device, serial in all_device_serials.items():
-                message = ""
-                if device not in st.session_state.devices:
-                    message = f"Found device: {device} with serial: {serial}"
-                    st.session_state.device_messages.append(message)
-                    st.session_state.devices.append(device)
-                    st.session_state.device_to_serial_map[device] = serial
-                else:
-                    message = f"Device {device} with serial: {serial} is already in the list."
-                    st.session_state.device_messages.append(message)
+        for device, serial in all_device_serials.items():
+            message = f"Found device: {device} with serial: {serial}" if device not in st.session_state.devices else f"Device {device} with serial: {serial} is already in the list."
+            st.session_state.device_messages.append(message)
+            if device not in st.session_state.devices:
+                st.session_state.devices.append(device)
+                st.session_state.device_to_serial_map[device] = serial
 
-            if matched_device:
-                st.write(
-                    f"Matching device for provided serial {device_serial} is {matched_device}")
-            else:
-                st.write("No matching devices found for the provided serial.")
-        else:
-            st.warning("Please enter both Device Serial and API Key.")
+        st.write(f"Matching device for provided serial {device_serial} is {matched_device}" if matched_device else "No matching devices found for the provided serial.")
+    elif not device_serial or not api_key:
+        st.warning("Please enter both Device Serial and API Key.")
 
     for message in st.session_state.device_messages:
         st.write(message)
 
-    selected_device = st.selectbox(
-        "Select a device:", st.session_state.devices)
-    if selected_device:
-        selected_device_first_char_capitalized = selected_device[0].upper()
-    else:
-        selected_device_first_char_capitalized = None
+    selected_device = st.selectbox("Select a device:", st.session_state.devices)
+    selected_device_first_char_capitalized = selected_device[0].upper() if selected_device else None
 
-    st.write("Select the date to fetch the total minute data consumption.")
-    selected_date = st.date_input("Select Date", datetime.now())
+    st.write("Select the date range to fetch the total minute data consumption.")
+    start_date = st.date_input("Start Date", datetime.now())
+    end_date = st.date_input("End Date", datetime.now())
 
-    if st.button("Fetch Consumption Data"):
-        if selected_device and device_serial and api_key:
-            selected_device_serial = st.session_state.device_to_serial_map.get(
-                selected_device)
-            if selected_device_serial:
-                consumption_data = get_consumption_data(
-                    selected_device_first_char_capitalized, selected_device_serial, device_serial, api_key, selected_date)
+    if st.button("Fetch Consumption Data") and selected_device and device_serial and api_key:
+        selected_device_serial = st.session_state.device_to_serial_map.get(selected_device)
+        if selected_device_serial:
+            with st.spinner('Fetching consumption data...'):
+                consumption_data = get_consumption_data(selected_device_first_char_capitalized, selected_device_serial, device_serial, api_key, start_date, end_date)
+            df = pd.DataFrame(consumption_data)
+            df['v1'] = df['v1'] / 10
+            df['frq'] = df['frq'] / 100
+            df['pt1'] = df['pt1'] / 10
+            df['pt2'] = df['pt2'] / 10
 
-                imported_energy_sum = 0
-                house_consumption_sum = 0
-                voltage_sum = 0
-                frequency_sum = 0
-                tank_temp1_sum = 0
-                tank_temp2_sum = 0
-                num_entries = 0
-                num_valid_temps = 0
-
-                for data in consumption_data:
-                    imported_energy_wh = joules_to_wh(data.get('imp', 0))
-                    house_consumption_w = data.get('hsk', 0)
-                    voltage_v1 = data.get('v1', None) / 10
-                    frequency = data.get('frq', None) / 100
-                    tank_temp1 = data.get('pt1', None)
-                    tank_temp2 = data.get('pt2', None)
-
-                    imported_energy_sum += imported_energy_wh
-                    house_consumption_sum += house_consumption_w
-                    voltage_sum += voltage_v1 if voltage_v1 is not None else 0
-                    frequency_sum += frequency if frequency is not None else 0
-
-                    if tank_temp1 is not None and tank_temp1 != 127:
-                        tank_temp1_sum += tank_temp1
-                        num_valid_temps += 1
-
-                    if tank_temp2 is not None and tank_temp2 != 127:
-                        tank_temp2_sum += tank_temp2
-
-                    num_entries += 1
-
-                avg_voltage = voltage_sum / num_entries if num_entries > 0 else 0
-                avg_frequency = frequency_sum / num_entries if num_entries > 0 else 0
-
-                if num_valid_temps > 0:
-                    avg_tank_temp1 = tank_temp1_sum / num_valid_temps
-                    avg_tank_temp2 = tank_temp2_sum / num_valid_temps
-                else:
-                    avg_tank_temp1 = 0
-                    avg_tank_temp2 = 0
-
-                total_house_consumption_wh = house_consumption_sum / 60
-                calculated_self_consumption = total_house_consumption_wh - imported_energy_sum
-
-                st.subheader("Summary:")
-                st.write("Total Imported Energy: {:.2f} Wh".format(
-                    imported_energy_sum))
-                st.write("Total House Consumption: {:.2f} Wh".format(
-                    total_house_consumption_wh))
-                st.write("Self Consumption: {:.2f} Wh".format(
-                    calculated_self_consumption))
-                st.write("Average Voltage: {:.2f} V".format(avg_voltage))
-                st.write("Average Frequency: {:.2f} Hz".format(avg_frequency))
-                st.write("Average Tank Temperature Sensor 1: {:.2f} °C".format(
-                    avg_tank_temp1))
-                st.write("Average Tank Temperature Sensor 2: {:.2f} °C".format(
-                    avg_tank_temp2))
-
-                # Create a DataFrame to hold the data for the bar chart
-                data = {
-                    'Metrics': ['Total Imported Energy (Wh)', 'Total House Consumption (Wh)', 'Self Consumption (Wh)'],
-                    'Values': [imported_energy_sum / 1000 , total_house_consumption_wh / 1000, calculated_self_consumption / 1000]
-                }
-
-                df = pd.DataFrame(data)
-
-                # Display the data as a bar chart
-                st.subheader("Summary Bar Chart:")
-                st.bar_chart(df.set_index('Metrics'))
-
-                st.subheader("Breakdown:")
-
+            # Columns for metrics in kWh (these values are the energy consumed in each minute)
+            df['imp_kWh_metric'] = df['imp'] / (60 * 1000)
+            df['exp_kWh_metric'] = df['exp'] / (60 * 1000)
+            df['gep_kWh_metric'] = df['gep'] / (60 * 1000)
+            if 'gen' in df.columns:
+                df['gen_kWh_metric'] = df['gen'] / (60 * 1000)
             else:
-                st.warning(
-                    "Could not find a serial number for the selected device.")
-        else:
-            st.warning(
-                "Please select a device, and ensure Device Serial and API Key are provided.")
+                df['gen_kWh_metric'] = 0
 
+            # Convert joules per minute to kW for display in the line chart
+            df['imp_kW'] = df['imp'] / 60000
+            df['exp_kW'] = df['exp'] / 60000
+            df['gep_kW'] = df['gep'] / 60000
+            if 'gen' in df.columns:
+                df['gen_kW'] = df['gen'] / 60000
+            else:
+                df['gen_kW'] = 0
+
+            # Sum the joules for each minute and then convert them to kWh for metrics
+            df['imp_total_joules'] = df['imp'].sum()
+            df['exp_total_joules'] = df['exp'].sum()
+            df['gep_total_joules'] = df['gep'].sum()
+            if 'gen' in df.columns:
+                df['gen_total_joules'] = df['gen'].sum()
+            else:
+                df['gen_total_joules'] = 0
+
+            df['imp_kWh_metric'] = df['imp_total_joules'] / 3600000
+            df['exp_kWh_metric'] = df['exp_total_joules'] / 3600000
+            df['gep_kWh_metric'] = df['gep_total_joules'] / 3600000
+            df['gen_kWh_metric'] = df['gen_total_joules'] / 3600000
+
+            # df['imp'] = df['imp'] / 60000  # Convert to kW
+            # df['exp'] = df['exp'] / 60000  # Convert to kW
+            # df['gep'] = df['gep'] / 60000  # Convert to kW
+            # df['gen'] = df['gen'] / 60000  # Convert to kW
+
+            df['hr'].fillna(0, inplace=True)
+            df['min'].fillna(0, inplace=True)
+            df['time'] = pd.to_datetime(df['yr'].astype(str) + '-' + df['mon'].astype(str) + '-' + df['dom'].astype(str) + ' ' +
+                                        df['hr'].astype(int).astype(str).str.zfill(2) + ':' +
+                                        df['min'].astype(int).astype(str).str.zfill(2), format='%Y-%m-%d %H:%M')
+            st.subheader("Summary:")
+            col1, col2 = st.columns(2)
+
+            st.subheader("Breakdown:")
+            st.line_chart(df.set_index('time')[['v1', 'frq']].rename(columns={'v1': 'Voltage', 'frq': 'Frequency Hz'}))
+
+            st.subheader("Tank Temperatures:")
+            st.line_chart(df.set_index('time')[['pt1', 'pt2']].rename(columns={'pt1': 'Tank 1 Temperature C', 'pt2': 'Tank 2 Temperature C'}))
+
+            st.subheader("Energy Graph:")
+            st.line_chart(df.set_index('time')[['imp_kW', 'exp_kW', 'gep_kW', 'gen_kW']].rename(columns={
+                            'imp_kW': 'Imported Energy kW', 'exp_kW': 'Exported Energy kW', 
+                            'gep_kW': 'Positive generation energy kW', 'gen_kW': 'Negative generation energy kW'}))
+
+            # CSV Export Button
+            if st.button('Export Data to CSV'):
+                csv = df.to_csv(index=False)
+                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+                href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+                st.markdown(href, unsafe_allow_html=True)
+
+            # Display metrics using either st.write() or col1.metric()
+            col1.metric("Total Imported Energy", "{:.2f} kWh".format(df['imp_kWh_metric'].iloc[0]))  # using iloc[0] to just get the single value from the dataframe
+            col1.metric("Total Exported Energy", "{:.2f} kWh".format(df['exp_kWh_metric'].iloc[0]))
+            col1.metric("Total Generated Energy", "{:.2f} kWh".format(df['gep_kWh_metric'].iloc[0]))
+            col1.metric("Total Consumed Energy", "{:.2f} kWh".format(df['gen_kWh_metric'].iloc[0]))
+
+            col2.metric("Average Voltage", "{:.2f} V".format(df['v1'].mean()))
+            col2.metric("Average Frequency", "{:.2f} Hz".format(df['frq'].mean()))
+            col2.metric("Average Tank Temperature Sensor 1", "{:.2f} °C".format(df['pt1'].mean()))
+            col2.metric("Average Tank Temperature Sensor 2", "{:.2f} °C".format(df['pt2'].mean()))
+
+        else:
+            st.warning("Could not find a serial number for the selected device.")
+    else:
+        st.warning("Please select a device, and ensure Device Serial and API Key are provided.")
 
 if __name__ == "__main__":
     main()
+
+
+
